e475a44ff4a722e483fa9fc15b81664d204e67e1
Jungwoo Yang <jungwoo@scalgo.com>
2015-11-22 21:40:44 +0100

    Change the interface of read/write function to be more generic

--- lru.h
+++ lru.h
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <list>
 #include <cassert>
+#include <algorithm>
 
 namespace bits {
 
@@ -19,19 +20,31 @@ struct Data {
 };
 
 template <typename IO>
-struct DirtyData : public Data<IO> {
-	bool dirty;
+struct DirtyData {
+	typedef typename IO::KeyType KeyType;
+	typedef std::list<DirtyData> DataList;
+	typedef std::unordered_map<KeyType, typename DataList::iterator> KeyMap;
+	typedef typename KeyMap::iterator KeyIterator;
+
+	DirtyData(): dirty(false) {}
+
+	void * data;
+	KeyIterator keyRef;
+	bool dirty:1;
+	size_t size:31;
 };
 
-template <typename IO, typename Data>
+template <typename IO, typename D, typename L>
 class LRUBase {
 private:
-	typedef typename IO::KeyType KeyType;
+	typedef D Data;
 	typedef typename Data::DataList DataList;
 	typedef typename Data::KeyMap KeyMap;
 	typedef typename KeyMap::iterator KeyIterator;
 	
 public:
+	typedef typename IO::KeyType KeyType;
+
 #ifndef NDEBUG
 	size_t hit, miss;
 #endif
@@ -48,19 +61,20 @@ public:
 
 	~LRUBase() {
 		for (auto & d : data) {
-			kick(d);
+			static_cast<L *>(this)->kick(d);
 			::operator delete(d.data);
 		}
 	}
 	
-	template <typename ValueType>
-	void read(const KeyType & key, ValueType & value) {
+	void read(const KeyType & key, void * value, size_t size) {
 		auto it = keys.find(key);
 		if (it != keys.end()) {
 #ifndef NDEBUG
 			hit++;
 #endif
-			value = *reinterpret_cast<ValueType *>(it->second->data);
+			std::copy(reinterpret_cast<const uint8_t*>(it->second->data), 
+					  reinterpret_cast<const uint8_t*>(it->second->data)+size, 
+					  reinterpret_cast<uint8_t*>(value));
 			data.splice(data.begin(), data, it->second);
 			return;
 		}
@@ -68,9 +82,103 @@ public:
 		miss++;
 #endif
 
-		io.read(key, value);
+		io.read(key, value, size);
 		
 		if (keys.size() >= capacity()) {
+			auto it = --data.end();
+			static_cast<L*>(this)->kick(*it);
+			::operator delete(it->data);
+			// reuse data space in back()
+			data.splice(data.begin(), data, it);
+			
+			KeyIterator old = it->keyRef;
+			keys.erase(old);
+		} else {
+			// Make sure the stored references to keys valid
+			// http://stackoverflow.com/questions/16781886/can-we-store-unordered-maptiterator
+			assert(keys.size() < (double)keys.max_load_factor() * keys.bucket_count());
+			data.emplace_front();
+		}
+		auto begin = data.begin();
+		begin->data = ::operator new(size);
+		std::copy(reinterpret_cast<const uint8_t *>(value), 
+				  reinterpret_cast<const uint8_t *>(value) + size, 
+				  reinterpret_cast<uint8_t *>(begin->data));
+		auto p = keys.emplace(key, begin);
+		begin->keyRef = p.first;
+	}
+
+	size_t capacity() const {
+		return _capacity;
+	}
+
+	size_t size() const {
+		return keys.size();
+	}
+
+	virtual void write(const KeyType & key, const void * value, size_t size) {}
+	
+private:
+	template <typename>
+	friend class ReadOnlyLRU;
+
+	template <typename>
+	friend class LRU;
+
+	virtual void kick(const Data & target) {}
+
+	const size_t _capacity;
+	DataList data;
+	KeyMap keys;
+	IO io;
+};
+
+}
+
+template <typename IO>
+class ReadOnlyLRU : public bits::LRUBase<IO, bits::Data<IO>, ReadOnlyLRU<IO>> {
+private:
+	typedef bits::LRUBase<IO, bits::Data<IO>, ReadOnlyLRU<IO>> LRUBase;
+	typedef typename LRUBase::Data Data;
+
+public:
+	typedef typename LRUBase::KeyType KeyType;
+	template <typename ...T>
+	ReadOnlyLRU(T... params) : LRUBase(params...) {}
+
+private:
+	template <typename, typename, typename>
+	friend class bits::LRUBase;
+
+	void kick(const Data & target) override {}
+};
+
+template <typename IO>
+class LRU : public bits::LRUBase<IO, bits::DirtyData<IO>, LRU<IO>> {
+private:
+	typedef bits::LRUBase<IO, bits::DirtyData<IO>, LRU<IO>> LRUBase;
+	typedef typename LRUBase::Data Data;
+	typedef typename Data::KeyIterator KeyIterator;
+
+public:
+	typedef typename LRUBase::KeyType KeyType;
+	template <typename ...T>
+	LRU(T... params) : LRUBase(params...) {}
+
+	virtual void write(const KeyType & key, const void * value, size_t size) override {
+		auto & keys = static_cast<LRUBase*>(this)->keys;
+		auto & data = static_cast<LRUBase*>(this)->data;
+		auto it = keys.find(key);
+		if (it != keys.end()) {
+			std::copy(reinterpret_cast<const uint8_t*>(value), 
+					  reinterpret_cast<const uint8_t*>(value)+size, 
+					  reinterpret_cast<uint8_t*>(it->second->data));
+			data.splice(data.begin(), data, it->second);
+			data.begin()->dirty = true;
+			return;
+		}
+
+		if (keys.size() >= LRUBase::capacity()) {
 			auto it = --data.end();
 			kick(*it);
 			::operator delete(it->data);
@@ -86,42 +194,25 @@ public:
 			data.emplace_front();
 		}
 		auto begin = data.begin();
-		begin->data = ::operator new(sizeof(value));
-		ValueType & storage = *reinterpret_cast<ValueType *>(begin->data);
-		
-		storage = value;
+		begin->data = ::operator new(size);
+		std::copy(reinterpret_cast<const uint8_t *>(value), 
+				  reinterpret_cast<const uint8_t *>(value) + size, 
+				  reinterpret_cast<uint8_t *>(begin->data));
 		auto p = keys.emplace(key, begin);
 		begin->keyRef = p.first;
+		data.begin()->dirty = true;
 	}
 
-	size_t capacity() const {
-		return _capacity;
-	}
-
-	size_t size() const {
-		return keys.size();
-	}
-	
 private:
-	virtual void kick(const Data & target) {}
+	template <typename, typename, typename>
+	friend class bits::LRUBase;
 
-	const size_t _capacity;
-	DataList data;
-	KeyMap keys;
-	IO io;
-};
+	void kick(const Data & target) override {
+		if (!target.dirty) return;
+		const KeyType & key = target.keyRef->first;
+		LRUBase::io.write(key, target.data, target.size);
+	}
 
-}
-
-template <typename IO>
-class ReadOnlyLRU : public bits::LRUBase<IO, bits::Data<IO>> {
-public:
-	typedef bits::LRUBase<IO, bits::Data<IO>> LRUBase;
-	template <typename ...T>
-	ReadOnlyLRU(T... params) : LRUBase(params...) {}
-private:
-	typedef bits::Data<IO> Data;
-	void kick(const Data & target) {}
 };
 
 #endif
--- test.cpp
+++ test.cpp
@@ -15,19 +15,23 @@ public:
 	  ,const std::vector<float> & l2)
 		: items1(l1), items2(l2) {} 
 	
-	void read(int x, int & s) const {
-		s = items1[x/2];
+	void read(int x, void * dest, size_t size) const {
+		if ((x % 2) == 0) {
+			int & d = *reinterpret_cast<int *>(dest);
+			d = items1[x/2];
+			return;
+		}
+		float & d = *reinterpret_cast<float *>(dest);
+		d = items2[x/2];
 	}
 	
-	void read(int x, float & s) const {
-		s = items2[x/2];
-	}
-	
-	void write(int x, const int & s) {
-		items1[x/2] = s;
-	}
-	
-	void write(int x, const float & s) {
+	void write(int x, void * src, size_t size) {
+		if ((x % 2) == 0) {
+			int & s = *reinterpret_cast<int *>(src);
+			items1[x/2] = s;
+			return;
+		}
+		float & s = *reinterpret_cast<float *>(src);
 		items2[x/2] = s;
 	}
 };
@@ -36,16 +40,16 @@ int main (int argc, char const *argv[])
 {
 	std::vector<int> a = {1,2,3,4};
 	std::vector<float> b = {1.1,1.2,1.3,1.4};
-	ReadOnlyLRU<IO> cache(4, a, b);
+	LRU<IO> cache(4, a, b);
 	std::vector<size_t> acc = {0,1,2,3,0,4,5,6,0,7};
 	int  c;
 	float d;
 	for (size_t i = 0 ; i < acc.size() ; ++i) {
 		if ((acc[i] % 2) == 0) {
-			cache.read(acc[i], c);
+			cache.read(acc[i], &c, sizeof(int));
 			std::cout << acc[i] << " -> " << c << ", ";
 		} else {
-			cache.read(acc[i], d);
+			cache.read(acc[i], &d, sizeof(float));
 			std::cout << acc[i] << " -> " << d << ", ";
 		}
 	}
